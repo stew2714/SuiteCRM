221f7e3ba42c2c787ab9124a113cb9e325bc9bc1
Ian Davie <ian.davie@salesagility.com>
2017-04-18 15:31:04 +0100

    server commit

--- config_override.php
+++ config_override.php
@@ -3,4 +3,9 @@
 $sugar_config['disable_persistent_connections'] = false;
 $sugar_config['invite_assignment'] = '63790586-7fb8-9c43-0c95-58eb85d85908';
 $sugar_config['email_xss'] = 'YToxMzp7czo2OiJhcHBsZXQiO3M6NjoiYXBwbGV0IjtzOjQ6ImJhc2UiO3M6NDoiYmFzZSI7czo1OiJlbWJlZCI7czo1OiJlbWJlZCI7czo0OiJmb3JtIjtzOjQ6ImZvcm0iO3M6NToiZnJhbWUiO3M6NToiZnJhbWUiO3M6ODoiZnJhbWVzZXQiO3M6ODoiZnJhbWVzZXQiO3M6NjoiaWZyYW1lIjtzOjY6ImlmcmFtZSI7czo2OiJpbXBvcnQiO3M6ODoiXD9pbXBvcnQiO3M6NToibGF5ZXIiO3M6NToibGF5ZXIiO3M6NDoibGluayI7czo0OiJsaW5rIjtzOjY6Im9iamVjdCI7czo2OiJvYmplY3QiO3M6MzoieG1wIjtzOjM6InhtcCI7czo2OiJzY3JpcHQiO3M6Njoic2NyaXB0Ijt9';
+$sugar_config['passwordsetting']['SystemGeneratedPasswordON'] = '0';
+$sugar_config['SAML_loginurl'] = '';
+$sugar_config['SAML_logouturl'] = '';
+$sugar_config['SAML_X509Cert'] = '';
+$sugar_config['authenticationClass'] = '';
 /***CONFIGURATOR***/
