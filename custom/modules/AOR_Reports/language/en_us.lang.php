64a39423ba9917184d5eaac88430ee4ffd1274f0
Kieran Monaghan <kdjmonaghan@gmail.com>
2017-05-01 13:58:05 +0100

    Vardefs and Layouts for Private Reporting

--- custom/modules/AOR_Reports/language/en_us.lang.php
+++ custom/modules/AOR_Reports/language/en_us.lang.php
@@ -3,4 +3,9 @@
 $mod_strings = array (
   'LBL_RUN_PREVIEW' => 'Run Preview',
   'LBL_DASHLET_VIEW_AS' => 'Run Report As',
+  'LBL_PRIVATE_GROUPS' => 'Private Groups',
+  'LBL_PRIVATE_REPORT_CHECKBOX' => 'Private Report',
+  'LBL_PRIVATE_USERS' => 'Private Users',
+  'LBL_PRIVATE_USER_OR_GROUP' => 'Users or Groups',
+  'LBL_SNAPSHOT_DATE' => 'Snapshot Date',
 );
