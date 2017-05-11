6b4c4d07b39e2e260b588a9815d005753034ac53
Ian <ian.davie@salesagility.com>
2017-05-11 16:00:07 +0100

    bugfix

--- custom/Extension/modules/AOR_Reports/Ext/Menus/maxtrixReport.php
+++ custom/Extension/modules/AOR_Reports/Ext/Menus/maxtrixReport.php
@@ -10,4 +10,4 @@
 if(!defined('sugarEntry') || !sugarEntry) die('Not A Valid Entry Point');
 
 global $current_language, $mod_strings;
-$module_menu[] = Array("index.php?module=AOR_Reports&action=maxtrixReport", $mod_strings['LBL_MATRIX_REPORT'],"AOR_Reports");
+$module_menu[] = Array("index.php?module=AOR_Reports&action=matrixReport", $mod_strings['LBL_MATRIX_REPORT'],"AOR_Reports");
