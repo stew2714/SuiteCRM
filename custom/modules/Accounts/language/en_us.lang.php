3d029912fca667ec42b21645ddcc78c39844acdf
Kieran Monaghan <kdjmonaghan@gmail.com>
2017-06-09 16:27:15 +0100

    Rename subpanels to Agreements instead of Contracts

---
+++ custom/modules/Accounts/language/en_us.lang.php
@@ -0,0 +1,5 @@
+<?php
+// created: 2017-06-09 14:38:43
+$mod_strings = array (
+  'AOS_Contracts' => 'Agreements',
+);
---
+++ custom/modules/Opportunities/language/en_us.lang.php
@@ -0,0 +1,5 @@
+<?php
+// created: 2017-06-09 14:42:55
+$mod_strings = array (
+  'AOS_Contracts' => 'Agreements',
+);
