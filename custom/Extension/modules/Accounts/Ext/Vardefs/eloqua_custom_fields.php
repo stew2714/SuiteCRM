3ae82a8705a840771d371f554a740d8fe6a2fd63
Kieran Monaghan <kdjmonaghan@gmail.com>
2017-07-11 17:26:35 +0100

    Change Account Fields to correct data structure - ELOQUA

--- custom/Extension/modules/Accounts/Ext/Vardefs/eloqua_custom_fields.php
+++ custom/Extension/modules/Accounts/Ext/Vardefs/eloqua_custom_fields.php
@@ -1,19 +1,37 @@
 <?php
 // Eloqua Record ID
-$dictionary['Account']['fields']['eloqua_id'] = array(
-    'name' => 'eloqua_id',
+$dictionary['Account']['fields']['eloqua_id_c'] = array(
+    'name' => 'eloqua_id_c',
     'vname' => 'LBL_ELOQUA_ID',
     'type' => 'int',
     'len' => 5,
     'default' => 0,
     'reportable' => false,
+    'source' => 'custom_fields',
 );
 
-$dictionary['Account']['fields']['rating'] = array (
-    'name' => 'rating',
+$dictionary['Account']['fields']['rating_c'] = array (
+    'name' => 'rating_c',
     'vname' => 'LBL_ELOQUA_ACCOUNT_RATING',
     'type' => 'varchar',
     'len' => '255',
+    'source' => 'custom_fields',
+);
+
+$dictionary['Account']['fields']['owner_sales_person'] = array(
+    'name' => 'owner_sales_person',
+    'vname' => 'LBL_OWNER_SALES_PERSON',
+    'type' => 'varchar',
+    'len' => '255',
+    'source' => 'custom_fields',
+);
+
+$dictionary['Account']['fields']['ownership'] = array(
+    'name' => 'ownership',
+    'vname' => 'LBL_OWNERSHIP',
+    'type' => 'varchar',
+    'len'  => '40',
+    'source' => 'custom_fields',
 );
 
 $dictionary['Account']['fields']['billing_address_country']['type']='enum';
