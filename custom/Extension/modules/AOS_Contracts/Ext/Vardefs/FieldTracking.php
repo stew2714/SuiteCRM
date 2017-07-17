eb5d59895e88c08459529e237529223e604ca612
Ian <ian.davie@salesagility.com>
2017-07-17 15:59:30 +0100

    added missing subpanels and relationships.

---
+++ custom/Extension/modules/AOS_Contracts/Ext/Vardefs/FieldTracking.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ian
+ * Date: 31/05/17
+ * Time: 15:50
+ */
+
+$dictionary["AOS_Contracts"]["fields"]["fieldtrackinghistory_agreements"] = array (
+    'name' => 'fieldtrackinghistory_agreements',
+    'type' => 'link',
+    'relationship' => 'fieldtrackinghistory_agreements',
+    'source' => 'non-db',
+    'module' => 'sa_Tracking_History',
+    'bean_name' => 'sa_Tracking_History',
+    'vname' => 'LBL_FIELD_TRACKING_HISTORY',
+);
+
