ca37c2bc25fb939cbc52afeae6d2a402dc28871d
Ian <ian.davie@salesagility.com>
2017-07-25 15:28:51 +0100

    added missing code from new view.

--- custom/modules/AOS_Contracts/views/view.create.php
+++ custom/modules/AOS_Contracts/views/view.create.php
@@ -53,7 +53,24 @@ class AOS_ContractsViewCreate extends ViewCreate
 
     public function display(){
 
-	    $this->loadRelated();
+        echo '<link rel="stylesheet" href="custom/modules/AOS_Contracts/css/modal.css">';
+        echo '<script src="custom/modules/AOS_Contracts/js/EditView.js" />';
+
+        global $current_user, $sugar_config;
+
+        $lockScript = '<script type="text/javascript" src="custom/modules/AOS_Contracts/js/Validation.js"></script>';
+        //define so nothing breaks
+
+        //we get the security groups so we can get all groups the current user is linked to.
+        $secGroups = BeanFactory::getBean("SecurityGroups");
+        $groups = $secGroups->getUserSecurityGroups($current_user->id);
+
+        $this->ss->assign('LOCK_FILES', $lockScript);
+        $tmpArray = $this->beanToArray();
+        $this->ss->assign('BEAN_DATA', "<script>var beanData = JSON.parse('" . json_encode($tmpArray) . "'); </script>");
+
+
+        $this->loadRelated();
         parent::display();
 
     }
@@ -71,5 +88,30 @@ class AOS_ContractsViewCreate extends ViewCreate
             }
 		}
 	}
+
+
+    function beanToArray(){
+        global $timedate, $current_user;
+        $tmpArray = $this->bean->toArray();
+        $tmpArray['today_month'] = date('m');
+        if($this->bean->previous_close_date_c != ""){
+            $tmpDate = $timedate->fromString($this->bean->previous_close_date_c);
+            $month = $tmpDate->format("m");
+            $tmpArray['previous_date_month'] = $month;
+        }
+        $user = $current_user->toArray();
+        $tmpArray['current_user'] = $user;
+        $secGroups = new SecurityGroup();
+        $groups = $secGroups->getUserSecurityGroups($current_user->id);
+        $i = 0;
+        foreach($groups as $key => $group){
+            $groups[$i] = $group['name'];
+            unset($groups[$key]);
+            $i++;
+        }
+        $tmpArray['current_user']['roles'] = $groups;
+        return $tmpArray;
+    }
+
 
 }
