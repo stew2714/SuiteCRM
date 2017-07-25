acb31355b200703287d89343ad7137c681aa9247
Ian <ian.davie@salesagility.com>
2017-07-25 15:18:20 +0100

    added Adwords Field into Leads module.

---
+++ custom/Extension/modules/Leads/Ext/Vardefs/adwords.php
@@ -0,0 +1,9 @@
+<?php
+
+$dictionary['Lead']['fields']['addwords_c'] = array (
+    'name' => 'addwords_c',
+    'vname' => 'LBL_ADDWORDS',
+    'type' => 'varchar',
+    'len' => '255',
+    'source' => 'custom_fields',
+);
