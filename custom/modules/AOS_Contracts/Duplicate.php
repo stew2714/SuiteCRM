ff458e7e58b9f415e5ebff06c88349dba6d08aef
Ian <ian.davie@salesagility.com>
2017-07-26 14:41:43 +0100

    added new relationship to handle documents when copied over. added logichook to start the handle of the copy.

--- custom/modules/AOS_Contracts/Duplicate.php
+++ custom/modules/AOS_Contracts/Duplicate.php
@@ -11,6 +11,47 @@ class duplicate {
 
 
     function activities($bean, $event, $arguments){
-        $var = "test";
+        //we need to fetch all the relating history+activities.
+        if(isset($bean->opportunity_id) && !empty($bean->opportunity_id)) {
+            $rel = "documents";
+            $opportunityBean = BeanFactory::getBean("Opportunities", $bean->opportunity_id);
+            $opportunityBean->load_relationship($rel);
+            $related = $opportunityBean->{$rel}->getBeans();
+            foreach ($related as $relatedBean) {
+                $toBeRelated = BeanFactory::getBean("Documents");
+                foreach ($toBeRelated->field_defs as $field => $defs) {
+                    if ($field != "id" && !empty($relatedBean->{$field})) {
+                        $toBeRelated->{$field} = $relatedBean->{$field};
+                    }
+                }
+
+                $id = create_guid();
+
+                //get revisions.
+                $relatedBean->load_relationship("revisions");
+                $revisions = $relatedBean->revisions->getBeans();
+                foreach($revisions as $revision){
+                    $documentRevision = BeanFactory::getBean("DocumentRevisions");
+                    foreach ($documentRevision->field_defs as $field => $defs) {
+                        if ($field != "id" && !empty($revision->{$field})) {
+                            $documentRevision->{$field} = $revision->{$field};
+                        }
+
+                        if($field == "id" && $toBeRelated->document_revision_id == $revision->{$field}){
+                            $revisionId = $revision->{$field};
+                        }
+
+                    }
+                    $documentRevision->document_id = $id;
+                    $documentRevision->save();
+                }
+
+                $toBeRelated->id = $id;
+                $toBeRelated->new_with_id = true;
+                $toBeRelated->document_revision_id = $revisionId;
+                $toBeRelated->aos_contracts_documents_1aos_contracts_ida = $bean->id;
+                $toBeRelated->save();
+            }
+        }
     }
 }
