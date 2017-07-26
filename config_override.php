8c03010fc6b861c492e9ba0db44e334436243394
Kieran Monaghan <kdjmonaghan@gmail.com>
2017-07-26 14:37:28 +0100

    Security Group IDs inside of config_override for this branch

--- config_override.php
+++ config_override.php
@@ -29,10 +29,10 @@ $sugar_config['AgreementsEmailTemplate'] = 'efa77cb1-819d-2a0a-0b78-597850cb15ef
 $sugar_config['eloqua_company'] = 'MModalIncSandbox';
 $sugar_config['eloqua_username'] = 'Kieran.Monaghan';
 $sugar_config['eloqua_password'] = 'SalesAgility01';
-$sugar_config['CommOps'] = '9d8e99e1-3f4f-a849-1200-5936c3de3e2f';
-$sugar_config['Sales'] = 'ce0b4da2-dbfa-a4d3-3d4f-5936d499f0df';
-$sugar_config['Legal'] = 'bb1bfe1b-1913-3110-e87f-594a9edcebea';
+$sugar_config['CommOps'] = '628f3c09-51d4-f65c-2afb-5936d572f62e';
+$sugar_config['Sales'] = '8cc7213b-1d28-12bc-7d6d-5936d546d416';
+$sugar_config['Legal'] = '628f3c09-51d4-f65c-2afb-5936d572f62e';
 $sugar_config['disable_persistent_connections'] = false;
-$sugar_config['Implementations'] = '2d00d5a1-496f-125a-654a-594cecaa1678';
+$sugar_config['Implementations'] = '90d40376-027d-a8f7-7cd3-593fb7ce674f';
 
 /***CONFIGURATOR***/
