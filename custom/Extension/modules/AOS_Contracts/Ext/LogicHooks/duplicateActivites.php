0fc301606c1681d38c0c6ea02ed7f012f17eacae
Ian <ian.davie@salesagility.com>
2017-07-27 09:10:59 +0100

    added scheduled task for US10.16 - will only fire if agreement has a opportunity which has related documents.

--- custom/Extension/modules/AOS_Contracts/Ext/LogicHooks/duplicateActivites.php
+++ custom/Extension/modules/AOS_Contracts/Ext/LogicHooks/duplicateActivites.php
@@ -2,5 +2,5 @@
 
 
 
-$hook_array['before_save'][] = Array(77, 'Duplicate Activities', 'custom/modules/AOS_Contracts/Duplicate.php',
+$hook_array['after_save'][] = Array(77, 'Duplicate Activities', 'custom/modules/AOS_Contracts/Duplicate.php',
     'duplicate', 'activities');
