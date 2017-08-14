6dfadb38c309aa6369bbc2c1dcc3d50835836a04
Ian <ian.davie@salesagility.com>
2017-08-14 12:20:22 +0100

    Merge branch 'QA' into US10_Merge_US12_US11
    # Conflicts:
    #	custom/modules/AOS_Contracts/js/EditView.js
    #	custom/modules/Accounts/language/en_us.lang.php
    #	custom/modules/Leads/language/en_us.lang.php
    #	custom/modules/Opportunities/js/Validation.js

--- custom/modules/Leads/language/en_us.lang.php
+++ custom/modules/Leads/language/en_us.lang.php
@@ -2,4 +2,6 @@
 // created: 2017-05-19 09:39:04
 $mod_strings = array (
   'LBL_EDITVIEW_PANEL1' => 'Eloqua Specific Fields',
+  'LBL_FIELD_TRACKING_HISTORY' => 'Field Tracking History',
+  'LBL_PLEASE_CONTACT' => 'Please contact Blythe Krzemien to move leads',
 );
