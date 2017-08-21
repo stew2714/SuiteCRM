c58239f66428b39136dc1ee0af8259126be27b1f
Ian <ian.davie@salesagility.com>
2017-08-21 11:30:37 +0100

    moved admin option to under users.

--- custom/Extension/modules/Administration/Ext/Administration/SharedRules.php
+++ custom/Extension/modules/Administration/Ext/Administration/SharedRules.php
@@ -7,21 +7,15 @@ $admin_option_defs['Administration']['sharedrules_settings'] = array(
     'LBL_SHAREDRULES_SETTINGS_DESC',
     './index.php?module=SharedSecurityRules&action=index'
 );
-if (isset($admin_group_header['sagility'])) {
-    $admin_option_defs['Administration'] =
+if (isset($admin_group_header[0])) {
+    $admin_option_defs =
         array_merge(
             (array)$admin_option_defs['Administration'],
-            (array)$admin_group_header['sagility'][3]['Administration']
+            (array)$admin_group_header[0][3]['Administration']
         );
 }
 
-$admin_group_header['sagility'] = array(
-    'LBL_SALESAGILITY_ADMIN',
-    '',
-    false,
-    $admin_option_defs,
-    ''
-);
+$admin_group_header[0][3]['Administration'] = $admin_option_defs;
 
 
 ?>
