72e4ae17c5898249f7df216a08ad2ecebd8a0294
Ian <ian.davie@salesagility.com>
2017-08-24 15:22:06 +0100

    updated condition check.

--- modules/SharedSecurityRules/SharedSecurityRules.php
+++ modules/SharedSecurityRules/SharedSecurityRules.php
@@ -285,7 +285,7 @@ class SharedSecurityRules extends Basic
                                     }
                                 }
                             }else{
-                                if( $condition->condition_operator == "AND" ){
+                                if( $condition->condition_operator !== "OR" && $result != false ){
                                     return true;
                                 }
                             }
