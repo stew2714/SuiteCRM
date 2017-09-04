72900e788bb85bfeb014a18a81f874b901a79425
Ian <ian.davie@salesagility.com>
2017-09-04 10:43:04 +0100

    added test line.

--- modules/SharedSecurityRules/SharedSecurityRules.php
+++ modules/SharedSecurityRules/SharedSecurityRules.php
@@ -253,7 +253,7 @@ class SharedSecurityRules extends Basic
                                 $result = false;
                             }
                         } else {
-                            if($condition->condition_operator !== "OR" && $result != false) {
+                            if($condition->condition_operator !== "OR" && $view != "view") {
                                 return true;
                             }
                         }
@@ -285,7 +285,7 @@ class SharedSecurityRules extends Basic
                                     }
                                 }
                             }else{
-                                if( $condition->condition_operator !== "OR" && $view != "view" && $result != false ){
+                                if( $condition->condition_operator !== "OR" && $view != "view" ){
                                     return true;
                                 }
                             }
