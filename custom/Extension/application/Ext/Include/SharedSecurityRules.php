e37554e54251464ee14416a2710985e99e25a5c3
Ian <ian.davie@salesagility.com>
2017-09-20 15:31:28 +0100

    remove modules from view.

--- custom/Extension/application/Ext/Include/SharedSecurityRules.php
+++ custom/Extension/application/Ext/Include/SharedSecurityRules.php
@@ -2,12 +2,16 @@
  //WARNING: The contents of this file are auto-generated
 $beanList['SharedSecurityRulesConditions'] = 'SharedSecurityRulesConditions';
 $beanFiles['SharedSecurityRulesConditions'] = 'modules/SharedSecurityRulesConditions/SharedSecurityRulesConditions.php';
-$moduleList[] = 'SharedSecurityRulesConditions';
+//$moduleList[] = 'SharedSecurityRulesConditions';
 $beanList['SharedSecurityRules'] = 'SharedSecurityRules';
 $beanFiles['SharedSecurityRules'] = 'modules/SharedSecurityRules/SharedSecurityRules.php';
 $moduleList[] = 'SharedSecurityRules';
 $beanList['SharedSecurityRulesActions'] = 'SharedSecurityRulesActions';
 $beanFiles['SharedSecurityRulesActions'] = 'modules/SharedSecurityRulesActions/SharedSecurityRulesActions.php';
-$moduleList[] = 'SharedSecurityRulesActions';
+//$moduleList[] = 'SharedSecurityRulesActions';
+
+
+$modInvisList[] = 'SharedSecurityRulesConditions';
+$modInvisList[] = 'SharedSecurityRulesActions';
 
 ?>
