2c46cf752423e8c378986962c3ea7ab69d654608
Ian <ian.davie@salesagility.com>
2017-10-12 08:14:37 +0100

    change of label.

--- custom/Extension/modules/Leads/Ext/Language/en_us.eloqua_id.php
+++ custom/Extension/modules/Leads/Ext/Language/en_us.eloqua_id.php
@@ -4,4 +4,4 @@ $mod_strings['LBL_ANNUAL_REVENUE'] = 'Annual Revenue';
 $mod_strings['LBL_NUMBER_OF_EMPLOYEES'] = 'Number of Employees';
 $mod_strings['LBL_INDUSTRY'] = 'Industry';
 $mod_strings['LBL_ELOQUA_LEAD_RATING'] = 'Lead Rating Combined';
-$mod_strings['LBL_ELOQUA_ACTIVITY'] = 'Eloqua Activity History';
+$mod_strings['LBL_ELOQUA_ACTIVITY'] = 'Eloqua Campaign History';
