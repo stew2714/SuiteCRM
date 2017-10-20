2ff7931b0518ab8c6f893e9e38f40ab7c8fe3e05
Ian <ian.davie@salesagility.com>
2017-10-20 15:09:35 +0100

    removed duplicate vardefs

--- custom/Extension/modules/Accounts/Ext/Vardefs/primary_campaign_c.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$dictionary['Account']['fields']['primary_campaign_c'] = array (
-    'name' => 'primary_campaign_c',
-    'vname' => 'LBL_PRIMARY_CAMPAIGN_FIELDS',
-    'type' => 'varchar',
-    'len' => '255',
-    'source' => 'custom_fields',
-);
--- custom/Extension/modules/Contacts/Ext/Vardefs/primary_campaign_c.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$dictionary['Contact']['fields']['primary_campaign_c'] = array (
-    'name' => 'primary_campaign_c',
-    'vname' => 'LBL_PRIMARY_CAMPAIGN_FIELDS',
-    'type' => 'varchar',
-    'len' => '255',
-    'source' => 'custom_fields',
-);
