cddf87c6555d8b6563540169afca7ef8134021cb
Apache <apache@phlprcmdmweb001.r1.corp.local>
2017-11-01 15:27:33 -0400

    vardef fixes for SOAP and new fields for Accounts

---
+++ custom/Extension/modules/Accounts/Ext/Vardefs/sugarfield_billing_address_city.php
@@ -0,0 +1,7 @@
+<?php
+ // created: 2017-11-01 18:15:16
+$dictionary['Account']['fields']['billing_address_city']['inline_edit']=true;
+$dictionary['Account']['fields']['billing_address_city']['comments']='The city used for billing address';
+$dictionary['Account']['fields']['billing_address_city']['merge_filter']='disabled';
+
+ ?>
