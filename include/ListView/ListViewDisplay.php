66209cb342299baedd27fd17cfbcc5a2fbc3aa36
Ian <ian.davie@salesagility.com>
2018-02-16 14:17:01 +0000

    added check to allow mass delete as part of Roles

--- include/ListView/ListViewDisplay.php
+++ include/ListView/ListViewDisplay.php
@@ -314,7 +314,8 @@ class ListViewDisplay {
 		foreach ( $this->actionsMenuExtraItems as $item )
 		    $menuItems[] = $item;
 
-		if(!$this->show_action_dropdown_as_delete) {
+        if ( ACLController::checkAccess($this->seed->module_dir,'delete',true) &&
+             !$this->show_action_dropdown_as_delete ) {
 			$menuItems[] = $this->buildDeleteLink($location);
 		}
 
