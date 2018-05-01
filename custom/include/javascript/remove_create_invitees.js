c26f30b4f721d89802926c08eb8880226a186716
michael <michael.crews@salesagility.com>
2018-05-01 13:47:25 +0100

    Remove create an invitee buttons from Call/Meetings

---
+++ custom/include/javascript/remove_create_invitees.js
@@ -0,0 +1,7 @@
+SUGAR.util.doWhen(
+  "$('#create_invitee_as_lead').length ",
+  function (){
+    $("#create-invitees").css("display","none");
+    $("#create-invitees").css("height","0px");
+  }
+);
