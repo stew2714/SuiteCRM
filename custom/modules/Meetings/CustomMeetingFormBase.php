5363daac30cb673bf5156fee73cb896f00a25411
Ian <ian.davie@salesagility.com>
2018-05-11 11:31:12 +0100

    BugFix, No email will be sent if email Template is not set, fix for ics file.

--- custom/modules/Meetings/CustomMeetingFormBase.php
+++ custom/modules/Meetings/CustomMeetingFormBase.php
@@ -156,7 +156,7 @@ class CustomMeetingFormBase extends MeetingFormBase
         $this->emailTemplate = new EmailTemplate();
         $this->emailTemplate->retrieve($sugar_config['MeetingCancelEmailTemplate']);
 
-        if (!$this->emailTemplate) {
+        if (empty($this->emailTemplate->id)) {
             $log->error('Email Template for notifying attendants of meeting cancellation not found in db.');
             return false;
         }
@@ -197,6 +197,8 @@ class CustomMeetingFormBase extends MeetingFormBase
 
         require_once("custom/modules/Meetings/vCal.php");
         $content = customvCal::get_ical_event($this->focus, $GLOBALS['current_user']);
+
+        $mailer->Ical = $content;
 
         if(file_put_contents($path,$content)){
            // $mailer->AddAttachment($path, 'meeting.ics', 'base64', 'text/calendar');
