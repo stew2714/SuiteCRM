3aebd6d3787d8860515dfc0c4dc094d2489d121f
michael <michael.crews@salesagility.com>
2018-05-14 17:12:28 +0100

    WIP - Add Agreement Number generation

---
+++ custom/Extension/modules/AOS_Contracts/Ext/Language/en_us.agreements_number_and_amendment_c.php
@@ -0,0 +1,2 @@
+<?php
+$mod_strings['LBL_AGREEMENTS_NUMBER_AND_AMENDMENT_C'] = 'Agreement No.';
---
+++ custom/Extension/modules/AOS_Contracts/Ext/Vardefs/agreements_number_and_amendment_c.php
@@ -0,0 +1,24 @@
+<?php
+$dictionary["AOS_Contracts"]["fields"]["agreements_number_and_amendment_c"] = array (
+    'name' => 'agreements_number_and_amendment_c',
+    'type' => 'varchar',
+    'len' => '12',
+    'source' => 'custom_fields',
+    'vname' => 'LBL_AGREEMENTS_NUMBER_AND_AMENDMENT_C',
+);
+
+$dictionary["AOS_Contracts"]["fields"]["agreements_number_c"] = array (
+    'name' => 'agreements_number_c',
+    'type' => 'int',
+    'len' => '8',
+    'source' => 'custom_fields',
+    'vname' => 'LBL_AGREEMENTS_NUMBER_C',
+);
+
+$dictionary["AOS_Contracts"]["fields"]["amendment_c"] = array (
+    'name' => 'amendment_c',
+    'type' => 'int',
+    'len' => '2',
+    'source' => 'custom_fields',
+    'vname' => 'LBL_AMENDMENT_C',
+);
