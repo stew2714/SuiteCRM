08c45c40d80b46b90e292d6721e3b6f0f4d1c705
Ian <ian.davie@salesagility.com>
2018-06-26 10:20:35 +0100

    changed dashlets to work with custom calendar dashlet

---
+++ custom/Extension/application/Ext/EntryPointRegistry/RetrieveDashPage.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ian
+ * Date: 26/06/18
+ * Time: 10:08
+ */
+
+
+
+$entry_point_registry['retrieve_dash_page'] = array(
+    'file' => 'custom/include/MySugar/retrieve_dash_page.php' ,
+    'auth' => true
+);
