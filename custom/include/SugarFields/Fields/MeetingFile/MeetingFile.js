8fa5b4ab67444990267fda95b6a3f51a07cd02c6
p.ernst <peter.ernst@salesagility.com>
2018-07-19 11:47:36 +0100

    Allow multiple attachments to show with name, from different folders

--- custom/include/SugarFields/Fields/MeetingFile/MeetingFile.js
+++ custom/include/SugarFields/Fields/MeetingFile/MeetingFile.js
@@ -1,31 +1,80 @@
 // $( document ).ready(function() {
 //YUI.Env.DOMReady
-  // Handler for .ready() called.
-SUGAR.util.doWhen("YUI.Env.DOMReady == true", function(){
-  if($("#EditView [name='record']").length > 0 ){
+// Handler for .ready() called.
+SUGAR.util.doWhen("YUI.Env.DOMReady == true", function () {
+  if ($("#EditView [name='record']").length > 0) {
     var record = $("#EditView [name='record']").val();
-  }else{
+  } else {
     var record = $("[name='record']").val();
   }
   doCall(record);
 
+  var $createFileArray = $('#file_create_file');
+  var $createFileClone = $createFileArray.clone()
+    .attr('id', 'file_create_file_origin');
+  $createFileArray.after($createFileClone);
+  $createFileArray.change(function (e) {
+    addAttachmentGroup(this);
+  });
 
+  var $uploadList = $("<ul>", {id: "upload_file_list"});
+  $createFileClone.before($uploadList);
+  $uploadList.append($createFileClone);
+  $uploadList.append($createFileArray);
+  $createFileClone.hide(0);
 
-  function doCall(record){
+  function doCall(record) {
     $.ajax({
       dataType: "json",
       url: "index.php?module=Meetings&action=fetchData",
       data: {
-        id : record,
+        id: record
       },
-      success: callback,
+      success: callback
     });
-  };
+  }
 
- });
+});
 
-function removeAttachment(record){
-  console.log(record);
+var attachmentGroupCounter = 0;
+
+/**
+ * @param obj
+ */
+function addAttachmentGroup(obj) {
+  var $obj = $(obj);
+  var $list = $("#upload_file_list");
+
+  var groupName = '';
+  for (var i = 0; i < obj.files.length; i++) {
+    if (i > 0) {
+      groupName += ', ';
+    }
+    groupName += obj.files[i].name;
+  }
+  var groupId = 'upload_file_list_' + attachmentGroupCounter;
+
+  var button = "<a href='#' onclick='$(\"#" + groupId
+    + "\")[0].remove(); return false;' class='button'>Remove</a> - ";
+  $list.append("<li id='" + groupId + "'>" + button
+    + "<a href=''>" + groupName + "</a></li>");
+
+  $obj.hide();
+  $obj.attr('id', $obj.attr('id') + '_' + attachmentGroupCounter);
+  $('#' + groupId).append($obj);
+
+  attachmentGroupCounter++;
+
+  var $createFileOrigin = $('#file_create_file_origin');
+  var $clone = $createFileOrigin.clone().attr('id', 'file_create_file').show();
+  $clone.change(function (e) {
+    addAttachmentGroup(this);
+  });
+  $list.append($clone);
+
+}
+
+function removeAttachment(record) {
   var deleteInput = "<input type='hidden' name='deleteMeetingAttachment[]' id='deleteMeetingAttachment-"
     + record + "' value='" + record + "'>";
   var $listRow = $('#file_list_' + record);
@@ -33,21 +82,21 @@ function removeAttachment(record){
   $listRow.hide(400);
 }
 
-var callback = function(data){
-  if(data.message == "success"){
-    $( data.results ).each(function( index, value ) {
+var callback = function (data) {
+  if (data.message == "success") {
+    $(data.results).each(function (index, value) {
       var button = "";
-      if( $("#EditView [name='record']").length > 0 || $("#CalendarEditView [name='record']").length > 0){
+      if ($("#EditView [name='record']").length > 0 || $("#CalendarEditView [name='record']").length > 0) {
         button = "<a href='#' onclick='removeAttachment( \"" + value.id + "\" );return false' class='button'>Remove</a> - ";
       }
-      $( "#file_create_file_list" ).append( "<li id='file_list_" + value.id + "'>" + button + "<a href='" + value.url + "'>" + value.name + "</a></li>" );
+      $("#file_create_file_list").append("<li id='file_list_" + value.id + "'>" + button + "<a href='" + value.url + "'>" + value.name + "</a></li>");
     });
   }
-}
+};
 
-var removeView = function(data){
-  if(data.message == "success"){
-      $( '#file_list_' + data.id ).hide();
+var removeView = function (data) {
+    if (data.message == "success") {
+      $('#file_list_' + data.id).hide();
+    }
   }
-}
 ;
