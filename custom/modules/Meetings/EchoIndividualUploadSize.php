befc526a98eef153781684c5b898deb554e78958
Dillon-Brown <dillon.brown@salesagility.com>
2018-08-06 19:04:09 +0100

    Fix meetings accounts search

--- custom/modules/Meetings/EchoIndividualUploadSize.php
+++ custom/modules/Meetings/EchoIndividualUploadSize.php
@@ -4,13 +4,11 @@ class EchoIndividualUploadSize
 {
     public function Output($event, $arguments)
     {
-        if (!empty($_REQUEST['sugar_body_only'])) {
-            return;
+        if(isset($_REQUEST['module']) && $_REQUEST['module'] == "Meetings" && empty($_REQUEST['sugar_body_only'])){
+            global $sugar_config;
+            $maxSize = empty($sugar_config['upload_individual_file_size'])
+                ? 0 : $sugar_config['upload_individual_file_size'];
+            echo "<script>var individualUploadMaxSize = $maxSize;</script>";
         }
-
-        global $sugar_config;
-        $maxSize = empty($sugar_config['upload_individual_file_size'])
-            ? 0 : $sugar_config['upload_individual_file_size'];
-        echo "<script>var individualUploadMaxSize = $maxSize;</script>";
     }
 }
