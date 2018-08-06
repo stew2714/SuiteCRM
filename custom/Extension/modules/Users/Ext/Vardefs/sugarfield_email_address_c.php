029089c8b025a1da49244909121628cc3d785a65
Dillon-Brown <dillon.brown@salesagility.com>
2018-08-06 17:58:43 +0100

    Fix fields

--- custom/Extension/modules/Users/Ext/Vardefs/sugarfield_email_address_c.php
+++ custom/Extension/modules/Users/Ext/Vardefs/sugarfield_email_address_c.php
@@ -1,6 +1,31 @@
 <?php
- // created: 2018-08-02 10:19:59
-$dictionary['User']['fields']['email_address_c']['inline_edit']='1';
-$dictionary['User']['fields']['email_address_c']['labelValue']='Email Address';
-$dictionary['User']['fields']['email_address_c']['source']='custom_fields';
- ?>
+// created: 2018-08-02 10:19:59
+$dictionary['User']['fields']['email_address_c'] = array(
+    'inline_edit' => '1',
+    'labelValue' => 'Email Address',
+    'source' => 'custom_fields',
+    'required' => false,
+    'name' => 'email_address_c',
+    'vname' => 'LBL_EMAIL_ADDRESS',
+    'type' => 'varchar',
+    'massupdate' => '0',
+    'default' => '',
+    'no_default' => false,
+    'comments' => '',
+    'help' => '',
+    'importable' => 'true',
+    'duplicate_merge' => 'disabled',
+    'duplicate_merge_dom_value' => '0',
+    'audited' => false,
+    'reportable' => true,
+    'unified_search' => false,
+    'merge_filter' => 'disabled',
+    'len' => '255',
+    'size' => '20',
+    'id' => 'Usersemail_address_c',
+    'custom_module' => 'Users',
+
+);
+$dictionary['User']['fields']['email_address_c']['labelValue'] = 'Email Address';
+$dictionary['User']['fields']['email_address_c']['source'] = 'custom_fields';
+?>
--- custom/Extension/modules/Users/Ext/Vardefs/sugarfield_exchange_version_c.php
+++ custom/Extension/modules/Users/Ext/Vardefs/sugarfield_exchange_version_c.php
@@ -1,6 +1,32 @@
 <?php
  // created: 2018-08-02 11:37:11
-$dictionary['User']['fields']['exchange_version_c']['inline_edit']='1';
+$dictionary['User']['fields']['exchange_version_c'] = array(
+            'inline_edit' => '1',
+            'labelValue' => 'Exchange Version',
+            'source' => 'custom_fields',
+            'required' => false,
+            'name' => 'exchange_version_c',
+            'vname' => 'LBL_EXCHANGE_VERSION',
+            'type' => 'multienum',
+            'massupdate' => '0',
+            'default' => '^NONE^',
+            'no_default' => false,
+            'comments' => '',
+            'help' => '',
+            'importable' => 'true',
+            'duplicate_merge' => 'disabled',
+            'duplicate_merge_dom_value' => '0',
+            'audited' => false,
+            'reportable' => true,
+            'unified_search' => false,
+            'merge_filter' => 'disabled',
+            'size' => '20',
+            'options' => 'exchange_list',
+            'studio' => 'visible',
+            'isMultiSelect' => true,
+            'id' => 'Usersexchange_version_c',
+            'custom_module' => 'Users',
+);
 $dictionary['User']['fields']['exchange_version_c']['labelValue']='Exchange Version';
 $dictionary['User']['fields']['exchange_version_c']['source']='custom_fields';
  ?>
