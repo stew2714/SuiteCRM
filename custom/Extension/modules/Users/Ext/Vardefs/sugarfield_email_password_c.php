b9a40d18224f4fc35dcffc41abda95bc5cc0dca1
Dillon-Brown <dillon.brown@salesagility.com>
2018-08-06 17:11:20 +0100

    Fix visible password field

--- custom/Extension/modules/Users/Ext/Vardefs/sugarfield_email_password_c.php
+++ custom/Extension/modules/Users/Ext/Vardefs/sugarfield_email_password_c.php
@@ -1,5 +1,6 @@
 <?php
  // created: 2018-08-02 10:20:18
+$dictionary['User']['fields']['email_password_c']['type'] = 'password';
 $dictionary['User']['fields']['email_password_c']['inline_edit']='1';
 $dictionary['User']['fields']['email_password_c']['labelValue']='Email Password';
 $dictionary['User']['fields']['email_password_c']['source']='custom_fields';
