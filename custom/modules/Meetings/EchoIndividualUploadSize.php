198552e616fdb03985568642382b85bc069274f0
Dillon-Brown <dillon.brown@salesagility.com>
2018-08-08 10:34:18 +0100

    Fixed maxuploadsize not working in the calendar

--- custom/modules/Meetings/EchoIndividualUploadSize.php
+++ custom/modules/Meetings/EchoIndividualUploadSize.php
@@ -4,7 +4,11 @@ class EchoIndividualUploadSize
 {
     public function Output($event, $arguments)
     {
-        if(isset($_REQUEST['module']) && $_REQUEST['module'] == "Meetings" && empty($_REQUEST['sugar_body_only'])){
+        if (!empty($_REQUEST['sugar_body_only'])) {
+            return;
+        }
+
+        if (isset($_REQUEST['module']) && $_REQUEST['module'] == "Meetings") {
             global $sugar_config;
             $maxSize = empty($sugar_config['upload_individual_file_size'])
                 ? 0 : $sugar_config['upload_individual_file_size'];
