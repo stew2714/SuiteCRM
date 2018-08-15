56d494505c54a5b6c21b85cf166ee7e2e4ca0e1d
Dillon-Brown <dillon.brown@salesagility.com>
2018-08-15 15:01:06 +0100

    MaxSize logging

--- custom/modules/Meetings/EchoIndividualUploadSize.php
+++ custom/modules/Meetings/EchoIndividualUploadSize.php
@@ -5,10 +5,12 @@ class EchoIndividualUploadSize
     public function Output($event, $arguments)
     {
         if (isset($_REQUEST['sugar_body_only'])) {
+            LoggerManager::getLogger()->fatal("UploadSize: sugar_body_only doesn't exist");
             return;
         }
 
         if (isset($_REQUEST['module']) && $_REQUEST['module'] == "Meetings" || isset($_REQUEST['module']) && $_REQUEST['module'] == "Calendar") {
+            LoggerManager::getLogger()->fatal("UploadSize: individualUploadMaxSize: " . $sugar_config['upload_individual_file_size']);
             global $sugar_config;
             $maxSize = empty($sugar_config['upload_individual_file_size'])
                 ? 0 : $sugar_config['upload_individual_file_size'];
