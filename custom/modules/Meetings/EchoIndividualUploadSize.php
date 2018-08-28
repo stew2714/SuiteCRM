dcc5ae4867b43b6d0820f9db1c3c955c6ede1d91
p.ernst <peter.ernst@salesagility.com>
2018-08-28 11:27:55 +0100

    Fix modules for outputting individual upload size

--- custom/modules/Meetings/EchoIndividualUploadSize.php
+++ custom/modules/Meetings/EchoIndividualUploadSize.php
@@ -2,6 +2,12 @@
 
 class EchoIndividualUploadSize
 {
+    public $modulesToOutputScript = [
+        'Meetings',
+        'Calendar',
+        'Home',
+    ];
+
     public function Output($event, $arguments)
     {
         if (!empty($_REQUEST['sugar_body_only'])) {
@@ -11,11 +17,23 @@ class EchoIndividualUploadSize
             return;
         }
 
-        if (isset($_REQUEST['module']) && $_REQUEST['module'] == "Meetings" || $_REQUEST['module'] == "Calendar") {
+        if ($this->isValidModule()) {
             global $sugar_config;
             $maxSize = empty($sugar_config['upload_individual_file_size'])
                 ? 0 : $sugar_config['upload_individual_file_size'];
             echo "<script>var individualUploadMaxSize = $maxSize;</script>";
         }
+    }
+
+    /**
+     * @return bool
+     */
+    protected function isValidModule()
+    {
+        if (!isset($_REQUEST['module'])) {
+            return false;
+        }
+
+        return in_array($_REQUEST['module'], $this->modulesToOutputScript, true);
     }
 }
